f2b79c29ec3d4f46a3d2362f6b12385adc2c5c6c
Max Jairo Collao <illapu1xyz@gmail.com>
2021-01-19 00:31:39 -0500

    This is a working app, even though there is no backend to make it work entirely

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import CommentForm from './components/CommentForm';
+import CommentList from './components/CommentList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    comments: [],
+    loading: false
+  }
+
+  addComment(comment) {
+    this.setState({
+      loading: false,
+      comments: [comment, ...this.state.comments]
+    });
+  }
+
+  async componentDidMount() {
+    // loading
+    this.setState({ loading: true });
+
+    // get all the comments
+    try{
+      const res = await fetch("http://localhost:7777")
+      const data = await res.json()
+      this.setState({
+        comments: data,
+        loading: false
+      });
+    } catch(err) {
+      this.setState({ loading: false });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App container bg-light shadow">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">
+            React Comments
+            <span role="img" aria-label="Chat">
+              💬
+            </span>
+          </h1>
+        </header>
+
+        <Container>
+          <Row className = "pt-3">
+            <Col xs={12} sm={4}>
+              <h6>Say something about React</h6>
+              <CommentForm addComment={this.addComment}/>
+            </Col>
+            <Col xs={12} sm={8}>
+            <CommentList
+              loading={this.state.loading}
+              comments={this.state.comments}
+            />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
